9ffe9f72d6e0730cba1481364130a0423137110a
Julio Berrocal <jberrocal2016@fau.edu>
2024-04-22 20:17:34 -0400

    menu prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -7,19 +7,26 @@ const mainCoursePrices = [25, 16.99, 12.99, 22];
 const dessertMenuPrices = [7, 6.99, 5.99, 4];
 
 const breakfastMenuItemsHTML = breakfastMenu
-  .map((item, index) => `<p>Item ${index + 1}: ${item} -$${breakfastPrices[index]}</p>`)
+  .map(
+    (item, index) =>
+      `<p>Item ${index + 1}: ${item} -$${breakfastPrices[index]}</p>`
+  )
   .join("");
 document.getElementById("breakfastMenuItems").innerHTML =
   breakfastMenuItemsHTML;
 
 let mainCourseItem = "";
 mainCourseMenu.forEach((item, index) => {
-  mainCourseItem += `<p>Item ${index + 1}: ${item} -$${mainCoursePrices[index]}</p>`;
+  mainCourseItem += `<p>Item ${index + 1}: ${item} -$${
+    mainCoursePrices[index]
+  }</p>`;
 });
 document.getElementById("maincourseMenuItems").innerHTML = mainCourseItem;
 
 let dessertItem = "";
 for (let i = 0; i < dessertMenu.length; i++) {
-  dessertItem += `<p>Item ${i+1}: ${dessertMenu[i]} -$${dessertMenuPrices[i]}</p>`;
+  dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]} -$${
+    dessertMenuPrices[i]
+  }</p>`;
 }
 document.getElementById("dessertMenuItems").innerHTML = dessertItem;
